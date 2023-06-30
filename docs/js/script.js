1225aa5ff7c6aef2520a540ef25379092feb243d
Hector Beltran <hectorbeltrancifuentes13@gmail.com>
2023-06-30 00:39:28 -0500

    Integración alertas, sesion integracion, index form

--- docs/js/script.js
+++ docs/js/script.js
@@ -85,11 +85,11 @@ function validarFormulario(){
     let validarCorreo = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
     if(nombre.value == "" || mensaje.value == null) {
-          alertas("Por favor ingrese su nombre");
+          alert("Por favor ingrese su nombre");
     }else if (correo.value == "" || mensaje.value == null) {
-      alertas("Por favor ingrese un correo"); 
+      alert("Por favor ingrese un correo"); 
     }else if(mensaje.value == "" || mensaje.value == null) {
-      alertas("Por favor ingrese su mensaje");
+      alert("Por favor ingrese su mensaje");
     }else if ( validarCorreo.test(correo.value) ) {  
       Swal.fire({
           title: 'Aceptado',
@@ -102,7 +102,7 @@ function validarFormulario(){
     correo.value = "";
     mensaje.value = "";
     }else {
-          alertas("Por favor, ingresa un correo electrónico válido.");
+          alert("Por favor, ingresa un correo electrónico válido.");
       }
   });
 };
