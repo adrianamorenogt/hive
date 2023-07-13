8e897964bcfd710872542f41a18ec02659bc183d
Hector Beltran <hectorbeltrancifuentes13@gmail.com>
2023-07-12 22:23:15 -0500

    Renderizado de imagenes e implementación de bootstrap

--- docs/js/script.js
+++ docs/js/script.js
@@ -16,28 +16,28 @@ window.addEventListener('scroll', function () {
   }
 });
 
-//Evento para menu hamburguesa  
-//Animation con js para menu
-document.querySelector(".burger_menu").addEventListener("click", animateBars);
-var line1 = document.querySelector(".line1_menu");
-var line2 = document.querySelector(".line2_menu");
-var line3 = document.querySelector(".line3_menu");
+// //Evento para menu hamburguesa  
+// //Animation con js para menu
+// document.querySelector(".burger_menu").addEventListener("click", animateBars);
+// var line1 = document.querySelector(".line1_menu");
+// var line2 = document.querySelector(".line2_menu");
+// var line3 = document.querySelector(".line3_menu");
 
-function animateBars() {
-  line1.classList.toggle("activeline1_menu");
-  line2.classList.toggle("activeline2_menu");
-  line3.classList.toggle("activeline3_menu");
+// function animateBars() {
+//   line1.classList.toggle("activeline1_menu");
+//   line2.classList.toggle("activeline2_menu");
+//   line3.classList.toggle("activeline3_menu");
 
-  //abrir y cerrar menu hamburguesa capturando el div nav-bar y cambiando su estilo de none a block
-  var navBar = document.querySelector(".nav-bar-web");
-  if (navBar.style.display === "block") {
-    navBar.style.display = "none";
-  } else {
-    navBar.style.display = "block";
-  }
-}
+//   //abrir y cerrar menu hamburguesa capturando el div nav-bar y cambiando su estilo de none a block
+//   var navBar = document.querySelector(".nav-bar-web");
+//   if (navBar.style.display === "block") {
+//     navBar.style.display = "none";
+//   } else {
+//     navBar.style.display = "block";
+//   }
+// }
 //ocultamos el menu lateral cuando el usuario seleccione una opción
-document.querySelector("ul").addEventListener("click", animateBars);
+//document.querySelector("ul").addEventListener("click", animateBars);
 
 //Funcionalidad y animación del slider
 var swiper = new Swiper(".mySwiper", {
